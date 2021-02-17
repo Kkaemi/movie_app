7963b6c3cf16d31f893ce56b9db1407df9695445
SeongHyeon Ji <kitys2k3@naver.com>
2021-02-17 20:25:40 +0900

    GitHub Page 배포

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -35,6 +36,8 @@ class App extends React.Component {
                 key={movie.id}
                 id={movie.id}
                 year={movie.year}
+                rating={movie.rating}
+                genres={movie.genres}
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 // prop 값을 받지 않으면 굳이 class component로 만들 필요 없음
-function Movie({ id, title, year, rating, genres, summary, poster }) {
+function Movie({ title, year, rating, genres, summary, poster }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <p className="movie__summary">{summary}</p>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
@@ -19,8 +27,8 @@ Movie.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired,
-  rating: PropTypes.number,
-  genres: PropTypes.array,
+  rating: PropTypes.number.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
 };
