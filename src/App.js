9c85e26e2bb4d11c7a3ff3d9b428992edce6b367
SeongHyeon Ji <kitys2k3@naver.com>
2021-02-17 18:30:41 +0900

    컴포넌트 life cycle method들이 언제 동작하는지 실습

--- src/App.js
+++ src/App.js
@@ -18,7 +18,14 @@ class App extends React.Component {
     // setState 함수를 실행하면 render 함수가 다시 실행된다.
     this.setState((current) => ({ count: current.count - 1 }));
   };
+  componentDidMount() {
+    console.log("component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
   render() {
+    console.log("I'm rendering");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
@@ -30,3 +37,31 @@ class App extends React.Component {
 }
 
 export default App;
+
+/*
+  React.Component는 life cycle method를 가진다.
+
+  <Mount>
+  constructor()
+  ->
+  static getDerivedStateFromProps()
+  ->
+  render()
+  ->
+  componentDidMount() 최초로 랜더링이 실행되고 나서 실행
+
+  <Update>
+  static getDerivedStateFromProps()
+  ->
+  shouldComponenetUpdate()
+  ->
+  render()
+  ->
+  getSnapshotBeforeUpdate()
+  ->
+  componentDidUpdate() 랜더링 업데이트가 되고 나서 실행
+
+  <Unmounting>
+  componentWillUnmount()
+
+*/
